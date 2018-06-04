eedf905d152f17399d93779fdc3f100fc20f0be3
suzujun <suzuki_junsuke@cyberagent.co.jp>
2018-06-04 10:37:41 +0900

    Added minus days parameter

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 
@@ -42,13 +43,24 @@ func main() {
 		fmt.Println("required blog name")
 		os.Exit(1)
 	}
-	run(os.Args[1])
+	var minusDaysIndex int
+	var minusDays int
+	for i, v := range os.Args {
+		if v == "-d" || v == "--minus_days" {
+			minusDaysIndex = i
+		} else if minusDaysIndex == i-1 {
+			if num, err := strconv.Atoi(v); err == nil {
+				minusDays = num
+			}
+		}
+	}
+	run(os.Args[1], minusDays)
 }
 
-func run(name string) {
+func run(name string, minusDays int) {
 	// url := "https://ameblo.jp/yotsuba-kids"
 	y, m, d := time.Now().Date()
-	// d -= 3
+	d -= minusDays
 	start := time.Date(y, m, d, 0, 0, 0, 0, jst)
 	end := time.Date(y, m, d+1, 0, 0, 0, -1, jst)
 	list, err := fetchArticleList(name)
@@ -69,19 +81,24 @@ func run(name string) {
 	}
 	fmt.Println("Which article do you want to download? Please choose a number.")
 	fmt.Println("You can specify more than one by separating with commas.")
+	selectionMap := make(map[string]bool, len(articles))
 	for i, article := range articles {
+		index := fmt.Sprint(i + 1)
+		selectionMap[index] = true
 		fmt.Printf("- %d: %s %s\n", i+1, article.CreatedAt.Format("2006-01-02 15:04:05"), article.Title)
 	}
 	fmt.Print("selection number: ")
-	var selectionNumber string
-	if _, err := fmt.Scan(&selectionNumber); err != nil {
+	var selection string
+	if _, err := fmt.Scan(&selection); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	numbers := strings.Split(selectionNumber, ",")
-	selectionMap := make(map[string]bool, len(numbers))
-	for _, n := range numbers {
-		selectionMap[n] = true
+	if selection != "all" {
+		numbers := strings.Split(selection, ",")
+		selectionMap = make(map[string]bool, len(numbers))
+		for _, n := range numbers {
+			selectionMap[n] = true
+		}
 	}
 	for i, article := range articles {
 		if _, ok := selectionMap[fmt.Sprint(i+1)]; !ok {
